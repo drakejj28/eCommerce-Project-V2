290221802852613dba352260b6527012db765253
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-07-23 15:32:18 -0400

    Added images not showing on deployment

--- server/server.cjs
+++ server/server.cjs
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/images', express.static(path.join(__dirname, 'src', 'images')));
 
 // app.use(cors())
 app.use(cors({
